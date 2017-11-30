f6b4af98c3e08a82ff78c9c3e6ae9258278149b4
Dongyoung_Kim <521ab@naver.com>
2017-11-30 17:34:26 +0900

    qwer

--- WPFSQLiteExample/MainWindow.xaml.cs
+++ WPFSQLiteExample/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFSQLiteExample.Models;
 
 namespace WPFSQLiteExample
 {
@@ -20,9 +24,95 @@ namespace WPFSQLiteExample
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string DBPath = "../../Assets/db.sqlite";
+
+        private SQLiteConnection Connection;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Connection = new SQLiteConnection(DBPath);
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            NameTable name = new NameTable
+            {
+                Name = InsertBox.Text
+            };
+
+            Connection.Insert(name);
+
+            Debug.WriteLine("Success Data Insert");
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var query = Connection.Table<NameTable>();
+
+            foreach (var data in query)
+            {
+                if (data.Name == UpdateBeforeBox.Text)
+                {
+                    Connection.RunInTransaction(() =>
+                    {
+                        Connection.Delete(data);
+                    });
+                }
+            }
+
+            Debug.WriteLine("Success Data Delete");
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var query = Connection.Table<NameTable>();
+
+            foreach (var data in query)
+            {
+                if (data.Name == UpdateBeforeBox.Text)
+                {
+                    data.Name = UpdateAfterBox.Text;
+
+                    Connection.RunInTransaction(() =>
+                    {
+                        Connection.Update(data);
+                    });
+                }
+            }
+
+            Debug.WriteLine("Success Data Update");
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            var query = Connection.Table<NameTable>();
+
+            ObservableCollection<NameTable> observableCollection = new ObservableCollection<NameTable>();
+
+            foreach (var data in query)
+            {
+                observableCollection.Add(new NameTable { NameId = data.NameId, Name = data.Name });
+            }
+
+            SelectedList.ItemsSource = observableCollection;
+
+            Debug.WriteLine("Success Data Select");
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Connection.CreateTable<NameTable>();
+
+            Debug.WriteLine("Success Create Table");
+        }
+
+        private void DropButton_Click(object sender, RoutedEventArgs e)
+        {
+            Connection.DropTable<NameTable>();
+
+            Debug.WriteLine("Success Drop Table");
         }
     }
 }
---
+++ WPFSQLiteExample/Models/NameTable.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSQLiteExample.Models
+{
+    public class NameTable
+    {
+        [PrimaryKey,NotNull,AutoIncrement]
+        public int NameId { get; set; }
+
+        [NotNull]
+        public string Name { get; set; }
+    }
+}
